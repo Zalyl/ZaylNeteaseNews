492a7c892290c8fa75004d28ffefcafd5207c472
大师 <chen1226810@qq.com>
2015-08-31 18:53:31 +0800

    搭建项目框架

--- Zayl--新闻/Zayl--新闻/Classes/News/News.h
+++ Zayl--新闻/Zayl--新闻/Classes/News/News.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  News.h
 //  Zayl--新闻
 //
 //  Created by 张玉 on 15/8/31.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface News : UITableViewController
 
 @end
-
